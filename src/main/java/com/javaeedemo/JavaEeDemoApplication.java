a4f8e9544198fbfaa05162223b3e188590a22737
Lunr_Lee <Lunr_Lee@outlook.com>
2022-09-22 10:36:59 +0800

    Initial commit

---
+++ src/main/java/com/javaeedemo/JavaEeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javaeedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEeDemoApplication.class, args);
+    }
+
+}
